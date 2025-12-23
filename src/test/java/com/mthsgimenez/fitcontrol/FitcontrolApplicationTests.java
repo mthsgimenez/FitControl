1f88e74f54a025f1054b59d39e9df94df3e8bef5
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2025-12-23 16:13:18 -0300

    feat: commit inicial

---
+++ src/test/java/com/mthsgimenez/fitcontrol/FitcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mthsgimenez.fitcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
