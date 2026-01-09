4b98afe7501253341aef596cb54730af3d64c489
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2026-01-09 16:59:11 -0300

    feat (OTPUtil): armazena e valida os códigos OTP

--- src/main/java/com/mthsgimenez/fitcontrol/util/OTPUtil.java
+++ src/main/java/com/mthsgimenez/fitcontrol/util/OTPUtil.java
@@ -1,35 +1,40 @@
 package com.mthsgimenez.fitcontrol.util;
 
 import lombok.Getter;
+import org.springframework.stereotype.Component;
 
 import java.security.SecureRandom;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class OTPUtil {
-    @Getter
-    private final String OTP;
-    @Getter
-    private final Instant expirationDate;
-    @Getter
-    private final Instant issuedAt;
+    private final static SecureRandom rnd = new SecureRandom();
 
-    private static final SecureRandom rnd = new SecureRandom();
+    private final static ConcurrentHashMap<String, OTP> OTPStorage = new ConcurrentHashMap<String, OTP>();
 
-    public OTPUtil() {
-        this.OTP = generateOTP();
-        this.issuedAt = Instant.now();
-        this.expirationDate = issuedAt.plus(Duration.of(5, ChronoUnit.MINUTES));
-    }
-
-    private static String generateOTP() {
+    public static OTP generateOTP() {
         StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < 6; i++) {
             result.append(rnd.nextInt(10));
         }
 
-        return result.toString();
+        return new OTP(result.toString());
+    }
+
+    public static void saveOTP(String key, OTP code) {
+        OTPStorage.put(key, code);
+    }
+
+    public static boolean verifyOTP(String key, String code) {
+        OTP otp = OTPStorage.get(key);
+        if (otp == null) return false;
+
+        Instant now = Instant.now();
+        return code.equals(otp.getCode())
+                && now.isBefore(otp.getExpirationDate())
+                && now.isAfter(otp.getIssueDate());
     }
 }
