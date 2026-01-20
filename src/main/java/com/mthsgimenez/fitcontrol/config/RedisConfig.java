1364a13ae4c27a703f7b0abf0fc5098abed0c619
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2026-01-20 17:14:56 -0300

    fix (RedisConfig): conflito entre os beans quando um RedisTemplate<String, String> é utilizado

--- src/main/java/com/mthsgimenez/fitcontrol/config/RedisConfig.java
+++ src/main/java/com/mthsgimenez/fitcontrol/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.mthsgimenez.fitcontrol.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
@@ -30,6 +31,7 @@ public class RedisConfig {
     }
 
     @Bean
+    @Primary
     public RedisTemplate<String, String> stringTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
