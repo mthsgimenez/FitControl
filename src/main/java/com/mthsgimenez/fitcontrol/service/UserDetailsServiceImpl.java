257034178eb7792b6b4c63c5d984ce4429abdc94
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2026-01-28 15:38:34 -0300

    fix (UserDetailsServiceImpl): falta de anotação causava StackOverflow ao chamar AuthenticationManager.authenticate()

--- src/main/java/com/mthsgimenez/fitcontrol/service/UserDetailsServiceImpl.java
+++ src/main/java/com/mthsgimenez/fitcontrol/service/UserDetailsServiceImpl.java
@@ -5,7 +5,9 @@ import org.jspecify.annotations.NonNull;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     UserRepository userRepository;
