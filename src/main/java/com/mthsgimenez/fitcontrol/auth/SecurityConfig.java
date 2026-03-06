408baa469c2e51a86c71a66a83b110ae9327f30a
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2026-03-06 09:08:08 -0300

    fix (Security): PreAuthorize não estava funcionando por falta de anotação em SecurityConfig

--- src/main/java/com/mthsgimenez/fitcontrol/auth/SecurityConfig.java
+++ src/main/java/com/mthsgimenez/fitcontrol/auth/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,6 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     private final JwtAuthenticationConverter jwtAuthenticationConverter;
